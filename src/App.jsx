4badfb2b8e332152027961301a97bee359b901ec
peterjanas <pejanas@gmail.com>
2024-12-10 12:57:59 +0100

    merrge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-    
+      <h1>Home</h1>
+      <p>This is what its all about</p> <br />
+      <p>lormekjlgshldvklerjlk</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
