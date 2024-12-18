6e95a8c84f6b659320c7db309109853f256b4aec
MASIH1903 <144231041+MASIH1903@users.noreply.github.com>
2024-12-18 17:02:22 +0100

    ændret desciption i home for hver kontinent cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,17 @@ function Home() {
     <>
   
      
-          <NavLink to="/world"><Card title="World" description="Test your knowledge across all continents and become a global expert!"/></NavLink>
+          <NavLink to="/world"><Card title="World" description="Identify countries, capitals and flags on a global scale - can you get them all?"/></NavLink>
        
-          <NavLink to="/europe"><Card title="Europe" description="Explore the rich history and vibrant cultures of Europe, from ancient times!"/></NavLink>
+          <NavLink to="/europe"><Card title="Europe" description=" Explore Europe by identifying its countries, capitals and flags."/></NavLink>
        
-          <NavLink to="/asia"><Card title="Asia" description="Discover the wonders of Asia, from ancient traditions to modern marvels!"/></NavLink>
+          <NavLink to="/asia"><Card title="Asia" description="Can you pinpoint countries, capitals and flags across vast Asia?"/></NavLink>
        
-          <NavLink to="/africa"><Card title="Africa" description="Dive into Africa’s stunning landscapes and rich heritage with stunning view!"/></NavLink>
+          <NavLink to="/africa"><Card title="Africa" description=" Identify the countries, capitals and flags of vibrant Africa."/></NavLink>
        
-          <NavLink to="/north-america"><Card title="North America" description="How well do you know the contrasts and cultures of North America?"/></NavLink>
+          <NavLink to="/north-america"><Card title="North America" description="Spot countries, capitals and flags in North America with confidence!"/></NavLink>
       
-          <NavLink to="/south-america"><Card title="South America" description="Challenge yourself with South America’s beauty and vibrant history!"/></NavLink>
+          <NavLink to="/south-america"><Card title="South America" description="How many South American countries, capitals and flags can you recall?"/></NavLink>
        
     </>
   );
